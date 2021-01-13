d24ce9eb329336f69149253230a496d1ffd5910a
Gokhnsby <48862948+Gokhnsby@users.noreply.github.com>
2021-01-13 20:53:13 +0300

    Add files via upload

---
+++ BaloonGame/Mover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+	//public GameObject trc;
+	public float speed;
+	
+	void Start ()
+	{
+		//Destroy (trc);
+
+	}
+	void Update (){
+
+		GetComponent<Rigidbody>().velocity = -1 * transform.forward * speed;		//In the speed changes of trucks and coin, it triggered in the prefab of the objects
+	}
+	
+}
